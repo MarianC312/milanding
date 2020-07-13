cad65a816cad90c832c2357c43c9692b75c02b89
MarianC312 <visual_design@live.com.ar>
2020-07-13 00:13:38 -0300

    quita log

--- app/main.jsx
+++ app/main.jsx
@@ -33,7 +33,6 @@ class Header extends React.Component {
     }
 
     render() {
-        console.log(this.state.toggleIcon);
         return (
             <div className="header">
                 <div className="container">
